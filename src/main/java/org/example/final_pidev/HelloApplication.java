58d44298e966bd0980b2c411f9c182407f72072f
Taieb <taieb.mohamed@esprit.tn>
2025-07-09 23:26:06 +0200

    trying to change the icon app pic

--- src/main/java/org/example/final_pidev/HelloApplication.java
+++ src/main/java/org/example/final_pidev/HelloApplication.java
@@ -3,6 +3,7 @@ package org.example.final_pidev;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,6 +11,14 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        // Load icon from resources (recommended approach)
+        try {
+            Image appIcon = new Image(getClass().getResourceAsStream("/image.jpg"));
+            stage.getIcons().add(appIcon);
+        } catch (Exception e) {
+            System.out.println("Could not load custom icon: " + e.getMessage());
+        }
+
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 400);
         stage.setTitle(" EDX ");
